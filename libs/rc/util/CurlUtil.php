492d748e7f127d0f56974f1816ca42fb527b0c5b
kazoo <kazoo@lno.co.jp>
2015-02-19 12:10:56 +0900

    util の追加

---
+++ libs/rc/util/CurlUtil.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package libs.util
+ */
+class CurlUtil
+{
+  public static function fileGetContents($path, $user = NULL, $password = NULL)
+  {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $path);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    if ($user && $password) {
+      curl_setopt($curl, CURLOPT_USERPWD, "$user:$password");
+    }
+    $result = curl_exec($curl);
+    curl_close($curl);
+
+    return $result;
+  }
+
+  public static function fileGetContentsBySFTP($path, $host, $user = NULL, $password = NULL)
+  {
+    $filePath = "sftp://$host$path";
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $filePath);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    if ($user && $password) {
+      curl_setopt($curl, CURLOPT_USERPWD, "$user:$password");
+    }
+    $result = curl_exec($curl);
+    curl_close($curl);
+
+    return $result;
+  }
+}
